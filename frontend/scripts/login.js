f6a0269eab6867bea1cf5f6d497863c99908cc2f
urbaneckkk <decaozao@gmail.com>
2025-11-21 15:04:17 -0300

    feat - criação da pagina de login

---
+++ frontend/scripts/login.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('loginForm');
+const errorMessage = document.getElementById('errorMessage');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const username = form.username.value.trim();
+    const password = form.password.value.trim();
+
+    if (!username || !password) {
+        errorMessage.textContent = 'Preencha todos os campos!';
+        return;
+    }
+
+    window.location.href = '../index.html';
+});
